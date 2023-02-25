bc9e95510da468c41b5f035b633294d821469df1
Lucas <lucas.schneider96@web.de>
2023-02-25 11:07:34 -0700

    proxies and request utils

---
+++ dashboard/config.py
@@ -0,0 +1,6 @@
+###################
+#Requests
+###################
+REQUESTS_SETTINGS = {'use_proxies':True}
+PROXIES_API_KEY = '94fe0971512480fd8e4bc39e85e7839e_sr98766_ooPq87'
+PROXIES = {'url': f"http://api.proxiesapi.com/?auth_key={PROXIES_API_KEY}&url="}
--- dashboard/okx_explorer.py
+++ dashboard/okx_explorer.py
@@ -1,15 +1,15 @@
-import requests
+#Import packages
+import json
+from bs4 import BeautifulSoup
+import os
+os.chdir(os.getcwd()+'\\dashboard')
+from utils import urllib_request
 
 
-#Get tx details
-
-
-
-
-
-
-
-
+#Get tx details -
+url = "https://www.oklink.com/api/explorer/v1/okexchain/transactions/0xF7823E8E07D214CDB1A9C5FCC32161798663DCEB6DD90512829D30E9DD12031A?t=1677347357302"
+additional_headers = {"x-apikey": "LWIzMWUtNDU0Ny05Mjk5LWI2ZDA3Yjc2MzFhYmEyYzkwM2NjfDI3ODg0NTg0Njg0MDU2NTQ="}
+urllib_request(url, additional_headers)
 
 
 #Get transaction for contract
