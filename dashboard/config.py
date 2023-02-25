e14942018625b67196df16a13d8e28a1af3ec90f
Lucas <lucas.schneider96@web.de>
2023-02-25 11:47:33 -0700

    tx hash details function

--- dashboard/config.py
+++ dashboard/config.py
@@ -4,3 +4,9 @@
 REQUESTS_SETTINGS = {'use_proxies':True}
 PROXIES_API_KEY = '94fe0971512480fd8e4bc39e85e7839e_sr98766_ooPq87'
 PROXIES = {'url': f"http://api.proxiesapi.com/?auth_key={PROXIES_API_KEY}&url="}
+
+
+###################
+#OKLINK
+###################
+OKLINK_API_KEY = "LWIzMWUtNDU0Ny05Mjk5LWI2ZDA3Yjc2MzFhYmEyYzkwM2NjfDI3ODg0NTg0Njg0MDU2NTQ"
--- dashboard/okx_explorer.py
+++ dashboard/okx_explorer.py
@@ -1,15 +1,44 @@
 #Import packages
-import json
-from bs4 import BeautifulSoup
+import time
 import os
+import pandas as pd
+import json
+
 os.chdir(os.getcwd()+'\\dashboard')
 from utils import urllib_request
 
+#Import config
+from config import OKLINK_API_KEY
 
 #Get tx details -
 url = "https://www.oklink.com/api/explorer/v1/okexchain/transactions/0xF7823E8E07D214CDB1A9C5FCC32161798663DCEB6DD90512829D30E9DD12031A?t=1677347357302"
-additional_headers = {"x-apikey": "LWIzMWUtNDU0Ny05Mjk5LWI2ZDA3Yjc2MzFhYmEyYzkwM2NjfDI3ODg0NTg0Njg0MDU2NTQ="}
-urllib_request(url, additional_headers)
+
+transaction_hash = '0xF7823E8E07D214CDB1A9C5FCC32161798663DCEB6DD90512829D30E9DD12031A'
+#Get transaction details from Oklink
+def get_transaction_details(transaction_hash, api_key = OKLINK_API_KEY, use_proxies = False):
+    """
+    Get transaction details from Oklink
+    @params:
+        transaction_hash   - Required  : transaction hash (str)
+        api_key            - Required  : Oklink api key (str)
+        use_proxies        - Required  : use proxies flag (bool)
+    """
+    #Create request
+    url = f"https://www.oklink.com/api/explorer/v1/okexchain/transactions/{transaction_hash}?t={time.time()}"
+    additional_headers = {"x-apikey": api_key}
+    
+    #Send request
+    tx_data = urllib_request(url, additional_headers, use_proxies = use_proxies)
+    
+    #Unwrap byte object
+    tx_data = json.loads(tx_data)
+
+    #Data wrangling
+    tx_data = tx_data['data']
+    tx_data.pop('inputData')
+    tx_data = pd.json_normalize(tx_data)
+
+    return tx_data
 
 
 #Get transaction for contract
