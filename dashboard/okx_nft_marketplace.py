cc11c43876298b48a40efdf4a75d19366fd29f9f
Lucas <lucas.schneider96@web.de>
2023-02-26 10:30:40 -0700

    improving nft data downloading

--- dashboard/okx_nft_marketplace.py
+++ dashboard/okx_nft_marketplace.py
@@ -28,19 +28,23 @@ def get_nft_transactions(cursor = '', use_proxies = False):
     #Unwrap byte object
     nft_data = json.loads(nft_data)
 
+
     #Data wrangling
     next_cursor = nft_data['data']['cursor']
+    next_flag = nft_data['data']['next']
     nft_data = nft_data['data']
     nft_data = nft_data['data']
     nft_data = pd.json_normalize(nft_data)
 
-    return {'data': nft_data, 'next_cursor': next_cursor}
+    return {'data': nft_data, 'next_cursor': next_cursor, 'next_flag': next_flag}
 
 #Get all nft transaction on the OKX marketplace
-def get_all_nft_transactions(max_lenght_dataframe = 100):
+def get_all_nft_transactions(max_length_dataframe = 100, temp_save = True):
     """
     Get all nft transaction on the OKX marketplace
     @params:
+    max_length_dataframe    - Required  : max length dataframe break value (int)
+    temp_save               - Required  : temporary save flag (bool)
     """
     #Init empty dataframe
     data = pd.DataFrame()
@@ -48,6 +52,7 @@ def get_all_nft_transactions(max_lenght_dataframe = 100):
     #First request
     nft_data_dict = get_nft_transactions(cursor = '', use_proxies = False)
     next_cursor = nft_data_dict['next_cursor']
+    next_flag = nft_data_dict['next_flag']
     nft_data = nft_data_dict['data']
     
     #Add data to data frame
@@ -56,8 +61,15 @@ def get_all_nft_transactions(max_lenght_dataframe = 100):
     #Loop requests
     while True:
         #First request
-        nft_data_dict = get_nft_transactions(cursor = next_cursor, use_proxies = False)
+        while True:
+            try:
+                nft_data_dict = get_nft_transactions(cursor = next_cursor, use_proxies = False)
+                break
+            except:
+                time.sleep(2)
+
         next_cursor = nft_data_dict['next_cursor']
+        next_flag = nft_data_dict['next_flag']
         nft_data = nft_data_dict['data']
         
         #Add data to data frame
@@ -66,15 +78,20 @@ def get_all_nft_transactions(max_lenght_dataframe = 100):
         print(len(data))
         
         #Final break condititons to get all data
-        if len(nft_data) < 10:
+        if len(nft_data) < 10 or not next_flag:
             break
 
         #Break condition for testing and getting data
-        if max_lenght_dataframe != '' and len(data) >= max_lenght_dataframe:
+        if max_length_dataframe != '' and len(data) >= max_length_dataframe:
             break
-    
+        
+        #Temp save
+        if temp_save and len(data) % 500 == 0:
+            print('Saving dataframe')
+            data.to_csv('temp_save_nft_data.csv')
+
     return data
 
 
-# nft_data_test = get_all_nft_transactions(max_lenght_dataframe = 1000)
+nft_data_test = get_all_nft_transactions(max_length_dataframe = '', temp_save = True)
 # nft_data_test.to_csv('NFT_data_test.csv')
